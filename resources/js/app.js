0aa7fa5ee05ce06bbb940f4306caeb803f106ade
anhdx <ilyboothu@gmail.com>
2020-02-19 08:40:25 +0700

    adding vuex

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import store from './store/index'
+
+Vue.component('posts', require('./components/Posts.vue'))
+Vue.component('createPost', require('./components/CreatePost.vue'))
+
+const app = new Vue({
+    el: '#app',
+    store
+});
